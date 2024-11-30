d4da69eecdf73116dca093e9a1fe2db2da657d9e
Ileska <ileska@tutanota.com>
2024-11-30 19:59:47 +0200

    Add one more test

--- tests.py
+++ tests.py
@@ -4,7 +4,12 @@ from random import randrange, seed
 
 from app import getObjectsCloserThan
 
+def setSeed(curSeed = randrange(0,int(1E8))):
+	seed(curSeed)
+	return curSeed
+
 def test_noCloser():
+	print(setSeed())
 	imageHeight = 10
 	imageWidth = 15
 	closerStreshHold = 2
@@ -16,6 +21,7 @@ def test_noCloser():
 	assert len(closer) == 0
 
 def test_oneInRange():
+	print(setSeed())
 	imageHeight = 10
 	imageWidth = 15
 	closerStreshHold = 2
@@ -32,6 +38,7 @@ def test_oneInRange():
 	assert len(closer) == 1
 
 def test_oneInCriticalRange():
+	print(setSeed())
 	imageHeight = 10
 	imageWidth = 15
 	closerStreshHold = 2
@@ -45,5 +52,23 @@ def test_oneInCriticalRange():
 
 	closer = getObjectsCloserThan(depthMap, closerStreshHold, criticalStreshHold, imageWidth, imageHeight)
 
+	print(closer)
 	assert closer == -1
 
+def test_manyInRange():
+	print(setSeed())
+	imageHeight = 10
+	imageWidth = 15
+	closerStreshHold = 2
+	criticalStreshHold = 1
+
+	depthMap = closerStreshHold+np.random.rand(imageHeight, imageWidth).flatten()* 10
+
+	rang = randrange(0,imageWidth)
+	for xCoord in range(rang):
+		yCoord = randrange(0,imageHeight)
+		depthMap[xCoord + imageWidth*yCoord] = (criticalStreshHold + closerStreshHold) / 2
+
+	closer = getObjectsCloserThan(depthMap, closerStreshHold, criticalStreshHold, imageWidth, imageHeight)
+
+	assert len(closer) == rang
